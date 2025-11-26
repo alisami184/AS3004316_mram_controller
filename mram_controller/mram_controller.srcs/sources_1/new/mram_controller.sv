76c683ea7f669398dadcabf43620fc6dde968641
aaithassou <ali.ait-hassou@lirmm.fr>
2025-11-26 13:45:29 +0100

    initial commit

---
+++ mram_controller/mram_controller.srcs/sources_1/new/mram_controller.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/14/2025 06:19:48 PM
+// Design Name: 
+// Module Name: mram_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mram_controller(
+
+    );
+endmodule
