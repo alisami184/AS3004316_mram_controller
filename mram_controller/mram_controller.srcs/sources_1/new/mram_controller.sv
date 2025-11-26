285aa88a5b4c75a9e301a189465d1251ab5741a8
aaithassou <ali.ait-hassou@lirmm.fr>
2025-11-26 14:07:21 +0100

    initialisation

--- mram_controller/mram_controller.srcs/sources_1/new/mram_controller.sv
+++ mram_controller/mram_controller.srcs/sources_1/new/mram_controller.sv
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: lirmm
+// Engineer: ali ait hassou
 // 
-// Create Date: 11/14/2025 06:19:48 PM
-// Design Name: 
+// Create Date: 26/11/2025 06:19:48 PM
+// Design Name: mram controller
 // Module Name: mram_controller
 // Project Name: 
 // Target Devices: 
@@ -21,6 +21,27 @@
 
 
 module mram_controller(
+// sys interface
+input  logic        sys_clk,
+input  logic        sys_rst,
 
+// data interface
+input  logic [15:0] wdata,
+input  logic [21:0] addr_i,
+input  logic        we,  
+output logic [15:0] rdata,
+
+
+// memory interface check as3004316 datasheet
+output logic        e,
+output logic        g,
+output logic        w,
+output logic [21:0] addr,
+output logic        ub,
+output logic        lb,
+output logic [15:0] data_out,
+output logic [15:0] data_in
     );
+    // fsm for write & read & signal control
+    
 endmodule
